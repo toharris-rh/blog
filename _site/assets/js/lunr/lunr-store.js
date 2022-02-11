ed7bd80cfde5391b4ed653b712ea2fd0efd1d54e
toharris-rh <toharris@redhat.com>
2022-02-11 08:45:13 -0800

    first post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "My Blog Site",
+        "excerpt":"Shout out to Keith Tenzer for writing the Blog that helps new people get their Blog site up with Github!  ","categories": ["Linux"],
+        "tags": ["Linux","Fedora","Blog"],
+        "url": "/blog/linux/First/",
+        "teaser": null
+      }]
